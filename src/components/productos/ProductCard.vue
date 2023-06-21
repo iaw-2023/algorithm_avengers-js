de2412ed36ac9fc9c8142a43460b0b263330329a
David López <dalo_vidpez@hotmail.com.ar>
2023-06-21 16:14:51 -0300

    Logrado interactuar con la API

---
+++ src/components/productos/ProductCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="card text-center" style="width: 18rem;">
+    <img :src="`${imagen}`" class="rounded mx-auto d-block" alt="Foto" id="foto">
+        <div class="card-body">
+            <h5 class="card-title">{{ nombre }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">$ {{ parseFloat(precio).toFixed(2) }}</h6>
+            <p class="card-text" id="descripcion">{{ descripcion }}</p>
+            <p class="card-text"><small class="text-body-secondary">Talles: {{ talles }}</small></p>
+            <p class="card-text"><small class="text-body-secondary">#{{ categoria }}</small></p>
+            <a href="#" class="btn btn-primary">
+                <v-icon name="bi-cart-plus" animation="wrench" hover scale="1.2" title="Añadir al carrito" />
+                Añadir al carrito
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        imagen: String,
+        nombre: String,
+        precio: String,
+        descripcion: String,
+        categoria: String,
+        talles: String
+    })
+</script>
+
+<style>
+#descripcion {
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+}
+
+#foto {
+    width:auto;
+    max-height: 250px;
+}
+
+</style>
---
+++ src/components/productos/ProductList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-for="prod in productos.data" :key="prod.id">
+        <ProductCard 
+            :imagen=prod.imagen
+            :nombre=prod.nombre 
+            :precio=prod.precio
+            :descripcion=prod.descripcion
+            :categoria=prod.categoria.nombre
+            :talles=prod.talles
+        />
+    </div>
+</template>
+
+<script>
+    import ProductCard from './ProductCard.vue';
+
+    export default{
+        components: {ProductCard},
+
+        data(){
+            return {
+                productos: [],
+            };
+        },
+
+        mounted(){
+            fetch("http://127.0.0.1:8000/rest/productos")
+                .then((response) => response.json())
+                .then((data) => (this.productos = data));
+        }
+    }
+</script>
+
+<style>
+</style>
