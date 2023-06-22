bed736b40dd705ab67a044fb8fe7a1fd988e0642
David López <dalo_vidpez@hotmail.com.ar>
2023-06-22 15:00:11 -0300

    Instalado Vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductosView from '../views/ProductosView.vue'
+import CarritoView from '../views/CarritoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +22,12 @@ const router = createRouter({
     {
       path: '/productos',
       name: 'productos',
-      component: () => import('../views/ProductosView.vue')
+      component: ProductosView
+    },
+    {
+      path: '/carrito',
+      name: 'carrito',
+      component: CarritoView
     }
   ]
 })
