f2ce910cf0512df97af055fe1e2d6a44118dd944
David López <dalo_vidpez@hotmail.com.ar>
2023-06-28 15:08:19 -0300

    Eliminado cuadro de búsqueda del NavBar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,36 +37,32 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menú</h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <RouterLink to="/" class="nav-link active">Home</RouterLink>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Productos
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-dark">
-                            <li @click="selectTodosProductos()"><RouterLink to="/productos" class="dropdown-item">Todos los productos</RouterLink></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li v-for="cat in categoryStore.getCategorias" :key="cat.id" @click="selectCategoria(cat.id)">
-                                <RouterLink to="/productos" class="dropdown-item">{{ cat.nombre }}</RouterLink>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink to="/about" class="nav-link active">About</RouterLink>
-                    </li>
-                </ul>
-                <form class="d-flex mt-3" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-success" type="submit">Buscar</button>
-                </form>
-            </div>
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menú</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <RouterLink to="/" class="nav-link active">Home</RouterLink>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Productos
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li @click="selectTodosProductos()"><RouterLink to="/productos" class="dropdown-item">Todos los productos</RouterLink></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li v-for="cat in categoryStore.getCategorias" :key="cat.id" @click="selectCategoria(cat.id)">
+                                    <RouterLink to="/productos" class="dropdown-item">{{ cat.nombre }}</RouterLink>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink to="/about" class="nav-link active">About</RouterLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
