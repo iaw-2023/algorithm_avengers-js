b8f46d620e375bf4c004a27106b21a08bf4a10bb
David López <dalo_vidpez@hotmail.com.ar>
2025-06-11 10:19:46 -0300

    Protected routes /pagar and /compras if the user is not authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -41,12 +41,14 @@ const router = createRouter({
     {
       path: '/compras',
       name: 'Mis compras',
-      component: ComprasView
+      component: ComprasView,
+      meta: {requiresAuth: true},
     },
     {
       path: '/pagar',
       name: 'Pagar con MercadoPago',
       component: MercadoPagoView,
+      meta: {requiresAuth: true}
     }
   ]
 });
@@ -54,7 +56,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore();
   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-      next('/login'); // Redirect to login if not authenticated
+      next('/');
   } else {
       next();
   }
